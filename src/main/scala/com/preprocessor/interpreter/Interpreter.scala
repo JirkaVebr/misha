f2453f86ab4992c980d1fbd4be976e3d331749e1
Jirka Vebr <jirkavebr@gmail.com>
2018-02-13 20:58:45 +0000

    Add a chainRun util method

--- src/main/scala/com/preprocessor/interpreter/Interpreter.scala
+++ src/main/scala/com/preprocessor/interpreter/Interpreter.scala
@@ -1,14 +1,40 @@
 package com.preprocessor.interpreter
 
+import com.preprocessor.ast.Ast
 import com.preprocessor.ast.Ast.Program
+import com.preprocessor.ast.Ast.Value.Value
+
+import scala.util.{Failure, Success, Try}
 
 class Interpreter(val program: Program) {
 
 	val rootEnvironment: RootEnvironment = createRootEnvironment()
 
-	def runProgram(): EvalState =
+	def runProgram(): Try[EvalState] =
 		StatementInterpreter.run(program.program)(EvalState(rootEnvironment))
 
 	private def createRootEnvironment(): RootEnvironment =
 		new RootEnvironment // TODO, this may get more involved
+}
+
+
+object Interpreter {
+
+	// TODO this isn't tail recursive, and so for longer lists this will be a total nightmare
+	def chainRun[V <: Ast.Node](items: scala.List[V], state: EvalState, evaluate: (V, EvalState) => Try[EvalState]):
+		Try[(scala.List[Value], EvalState)] = items match {
+		case Nil => Success((scala.List[Value](), state))
+		case expression :: tail =>
+			val evaluationResult = evaluate(expression, state)
+			evaluationResult match {
+				case Failure(reason) => Failure(reason)
+				case Success(newEvalState) =>
+					val subsequent = chainRun(tail, newEvalState, evaluate)
+					subsequent match {
+						case Failure(exception) => Failure(exception)
+						case Success(value) => Success((newEvalState.value :: value._1, value._2))
+					}
+			}
+	}
+
 }
