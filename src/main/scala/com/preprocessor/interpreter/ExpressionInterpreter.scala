810998462ecc12ebee570d7209d9948a45b3954a
Jirka Vebr <jirkavebr@gmail.com>
2018-02-13 01:30:46 +0000

    Add an unfinished, rudimentary expression interpreter

---
+++ src/main/scala/com/preprocessor/interpreter/ExpressionInterpreter.scala
@@ -0,0 +1,15 @@
+package com.preprocessor.interpreter
+
+import com.preprocessor.ast.Ast.Expression._
+import com.preprocessor.ast.Ast.Term.Term
+
+object ExpressionInterpreter {
+
+	def run(expression: Expression)(implicit state: EvalState): EvalState = expression match {
+		case BinaryOperation(operator, left, right) => sys.error("todo") // TODO
+		case UnaryOperation(operator, value) => sys.error("todo") // TODO
+		case Conditional(condition, consequent, alternative) => sys.error("todo") // TODO
+		case StringInterpolation(components) => sys.error("todo") // TODO
+		case term: Term => TermInterpreter.run(term)
+	}
+}
