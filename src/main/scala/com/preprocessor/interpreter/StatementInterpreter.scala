73be7288a6801d68a8bf2cfcaa1ab560ca3f837f
Jirka Vebr <jirkavebr@gmail.com>
2018-02-13 20:59:59 +0000

    Change interpreter failure handling and extend term interpreter a bit

--- src/main/scala/com/preprocessor/interpreter/StatementInterpreter.scala
+++ src/main/scala/com/preprocessor/interpreter/StatementInterpreter.scala
@@ -3,11 +3,15 @@ package com.preprocessor.interpreter
 import com.preprocessor.ast.Ast.Expression.Expression
 import com.preprocessor.ast.Ast.Statement._
 
+import scala.util.{Success, Try}
+
 object StatementInterpreter {
 
-	def run(statement: Statement)(implicit state: EvalState): EvalState = statement match {
-		case NoOp => state
-		case Sequence(current, following) => run(following)(run(current))
+	def run(statement: Statement)(implicit state: EvalState): Try[EvalState] = statement match {
+		case NoOp => Success(state)
+		case Sequence(current, following) =>
+			//run(following)(run(current))
+			sys.error("todo")
 
 		case TypeAliasDeclaration(alias, subType) => sys.error("todo") // TODO
 		case VariableDeclaration(name, typeAnnotation, value) => sys.error("todo") // TODO
--- src/main/scala/com/preprocessor/interpreter/TermInterpreter.scala
+++ src/main/scala/com/preprocessor/interpreter/TermInterpreter.scala
@@ -1,35 +1,70 @@
 package com.preprocessor.interpreter
 
-import com.preprocessor.ast.Ast.Term.{FunctionCall, MemberAccess, Term, Variable}
-import com.preprocessor.ast.Ast.{Term, Type, Value}
+import com.preprocessor.ast.Ast.Expression.Expression
+import com.preprocessor.ast.Ast.Term._
 import com.preprocessor.ast.Ast.Value._
+import com.preprocessor.ast.Ast.{Term, Type, Value}
+import com.preprocessor.ast.UnitOfMeasure.{GenericUnit, Percentage, Scalar}
+import com.preprocessor.error.ProgramError
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 object TermInterpreter {
 
 	def run(term: Term)(implicit state: EvalState): Try[EvalState] = term match {
 		case value: Value => value match {
-			case Value.Unit => state ~> (Type.Unit, Value.Unit)
+			case Value.Unit => state ~> Value.Unit
 			case primitiveValue: Primitive => primitiveValue match {
-				case Number(number, unit) => sys.error("todo") // TODO check dimensions
-				case String(string) => sys.error("todo") // TODO replace &
-				case _ => state ~> (primitiveValue.valueType, primitiveValue)
+				case number: Number => runNumber(number)
+				case _ => state ~> primitiveValue
 			}
 			case compositeValue: Composite => compositeValue match {
-				case Tuple2Value(first, second) => sys.error("todo") // TODO
-				case List(values) => sys.error("todo") // TODO find common supertype
+				case tuple: Tuple2Value => runTuple(tuple)
+				case list: Value.List => runListValue(list)
 			}
 		}
-		case Variable(name, variableType) => {
-			val variableValue = state.environment.lookup(name)
-			variableValue match {
-				case Some(x) => sys.error("todo") // TODO
-				case None => sys.error("todo") // TODO
-			}
+		case symbol: MagicSymbol => symbol match {
+			case ParentSelector => sys.error("todo") // TODO
 		}
+		case variable: Variable => runVariable(variable)
 		case FunctionCall(function, arguments) => sys.error("todo") // TODO
-		case Term.List(items) => sys.error("todo") // TODO
+		case list: Term.List => runListTerm(list)
 		case MemberAccess(container, name) => sys.error("todo") // TODO
 	}
+
+	private def runTuple(tuple: Tuple2Value)(implicit state: EvalState): Try[EvalState] = {
+		sys.error("todo") // TODO
+	}
+
+	private def runNumber(number: Number)(implicit state: EvalState): Try[EvalState] = {
+		val unit = number.unit
+		unit match {
+			case GenericUnit(subUnitProduct) => sys.error("todo") // TODO check dimensions
+			case Percentage() => state ~> (Type.Percentage, number)
+			case Scalar() => state ~> (Type.Number, number) // TODO It's not necessarily Type.Number
+		}
+	}
+
+	private def runVariable(variable: Variable)(implicit state: EvalState): Try[EvalState] = {
+		val variableValue = state.environment.lookup(variable.name)
+
+		variableValue match {
+			case Some(value) => state ~> value
+			case None => Failure(ProgramError(ProgramError.UndefinedVariable, variable))
+		}
+	}
+
+	private def runListValue(list: Value.List)(implicit state: EvalState): Try[EvalState] = {
+		sys.error("todo") // TODO find common supertype
+	}
+
+	private def runListTerm(list: Term.List)(implicit state: EvalState): Try[EvalState] = {
+		val chainResult = Interpreter.chainRun[Expression](list.items.toList, state, ExpressionInterpreter.run(_)(_))
+
+		chainResult match {
+			case Failure(exception) => Failure(exception)
+			case Success((items, finalState)) => runListValue(Value.List(items))(finalState)
+		}
+	}
+
 }
