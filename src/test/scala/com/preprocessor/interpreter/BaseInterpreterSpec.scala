4181f62f7df6f2f9cbba13575785354a0b2ae285
Jirka Vebr <jirkavebr@gmail.com>
2018-02-13 23:16:20 +0000

    Setup base interpreter spec

--- src/test/scala/com/preprocessor/interpreter/BaseInterpreterSpec.scala
+++ src/test/scala/com/preprocessor/interpreter/BaseInterpreterSpec.scala
@@ -4,4 +4,8 @@ import com.preprocessor.BaseSpec
 
 class BaseInterpreterSpec extends BaseSpec {
 
+	val testEnvironment: Environment = new Environment()
+
+	implicit val state: EvalState = EvalState(testEnvironment)
+
 }
