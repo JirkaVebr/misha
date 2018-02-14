eb0784bfd2955bf3b7a1d3ed021516c47bb1acba
Jirka Vebr <jirkavebr@gmail.com>
2018-02-14 04:36:10 +0000

    EvalState: more utility functions

--- src/main/scala/com/preprocessor/interpreter/EvalState.scala
+++ src/main/scala/com/preprocessor/interpreter/EvalState.scala
@@ -15,6 +15,12 @@ case class EvalState(environment: Environment, value: Value.Value = Value.Unit, 
 	@inline def ~>(newState: (Environment, Value.Value)): Success[EvalState] =
 		Success(EvalState(newState._1, newState._2, newState._2.valueType))
 
+	@inline def withNewType(newType: Type.Any): Success[EvalState] =
+		Success(EvalState(environment, value, newType))
+
+	@inline def withNewValue(newValue: Value.Value, newType: Type.Any): Success[EvalState] =
+		Success(EvalState(environment, newValue, newType))
+
 	def fail(errorCode: ProgramErrorCode, nodes: Ast.Node*): Failure[EvalState] =
 		Failure(ProgramError(errorCode, this, (if (nodes.isEmpty) List(value) else nodes): _*))
 
