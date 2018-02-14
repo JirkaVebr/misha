d31b92543eb6dc1d5ed863797b8131c64624434b
Jirka Vebr <jirkavebr@gmail.com>
2018-02-14 01:50:22 +0000

    Change evalState creation methods

--- src/main/scala/com/preprocessor/interpreter/EvalState.scala
+++ src/main/scala/com/preprocessor/interpreter/EvalState.scala
@@ -7,10 +7,13 @@ import com.preprocessor.error.ProgramError.ProgramErrorCode
 
 import scala.util.{Failure, Success, Try}
 
-case class EvalState(environment: Environment, nodeType: Type.Any = Type.Any, value: Value.Value = Value.Unit) {
+case class EvalState(environment: Environment, value: Value.Value = Value.Unit, nodeType: Type.Any = Type.Any) {
 
 	@inline def ~>(value: Value.Value): Try[EvalState] =
-		Success(EvalState(environment, value.valueType, value))
+		Success(EvalState(environment, value, value.valueType))
+
+	@inline def ~>(newState: (Environment, Value.Value)): Try[EvalState] =
+		Success(EvalState(newState._1, newState._2, newState._2.valueType))
 
 	def fail(errorCode: ProgramErrorCode, nodes: Ast.Node*): Failure[EvalState] =
 		Failure(ProgramError(errorCode, this, (if (nodes.isEmpty) List(value) else nodes): _*))
