70ff0e45e194ac9ee7f95449c959e417e5159cf2
Jirka Vebr <jirkavebr@gmail.com>
2018-02-14 03:38:08 +0000

    EvalState: more specific type annotation

--- src/main/scala/com/preprocessor/interpreter/EvalState.scala
+++ src/main/scala/com/preprocessor/interpreter/EvalState.scala
@@ -2,17 +2,17 @@ package com.preprocessor.interpreter
 
 import com.preprocessor.ast.Ast
 import com.preprocessor.ast.Ast.{Type, Value}
-import com.preprocessor.error.{Error, ProgramError}
 import com.preprocessor.error.ProgramError.ProgramErrorCode
+import com.preprocessor.error.{Error, ProgramError}
 
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Success}
 
 case class EvalState(environment: Environment, value: Value.Value = Value.Unit, nodeType: Type.Any = Type.Any) {
 
-	@inline def ~>(value: Value.Value): Try[EvalState] =
+	@inline def ~>(value: Value.Value): Success[EvalState] =
 		Success(EvalState(environment, value, value.valueType))
 
-	@inline def ~>(newState: (Environment, Value.Value)): Try[EvalState] =
+	@inline def ~>(newState: (Environment, Value.Value)): Success[EvalState] =
 		Success(EvalState(newState._1, newState._2, newState._2.valueType))
 
 	def fail(errorCode: ProgramErrorCode, nodes: Ast.Node*): Failure[EvalState] =
