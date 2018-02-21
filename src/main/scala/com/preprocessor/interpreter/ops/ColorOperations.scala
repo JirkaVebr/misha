0823c08fa7b959538da8ec1a63d0822076a2b3b3
Jirka Vebr <jirkavebr@gmail.com>
2018-02-22 00:06:55 +0100

    Add simple color ops object

---
+++ src/main/scala/com/preprocessor/interpreter/ops/ColorOperations.scala
@@ -0,0 +1,20 @@
+package com.preprocessor.interpreter.ops
+
+import com.preprocessor.ast.Ast.Value.Rgba
+
+object ColorOperations {
+
+	private def combineColors(x: Rgba, y: Rgba, combine: (Int, Int) => Int, normalize: (Int) => Int): Rgba =
+		Rgba(
+			normalize(combine(x.r, y.r)),
+			normalize(combine(x.g, y.g)),
+			normalize(combine(x.b, y.b)),
+			normalize(combine(x.a, y.a))
+		)
+
+
+	def addColors(x: Rgba, y: Rgba): Rgba = combineColors(x, y, _ + _, _ min 255)
+
+	def subtractColors(x: Rgba, y: Rgba): Rgba = combineColors(x, y, _ - _, _ max 0)
+
+}
