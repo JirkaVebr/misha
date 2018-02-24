3201abff0dcc7ee0bf8f0b4007431cddf3dfe221
Jirka Vebr <jirkavebr@gmail.com>
2018-02-24 17:27:53 +0100

    Subtyping random stuff

--- src/main/scala/com/preprocessor/interpreter/typing/Subtype.scala
+++ src/main/scala/com/preprocessor/interpreter/typing/Subtype.scala
@@ -5,9 +5,10 @@ import com.preprocessor.ast.Ast.Type
 object Subtype {
 
 	// TODO (when this is done, uncomment the test in StatementInterpreterSpec)
-	def isSubtypeOf(left: Type.Any, right: Type.Any): Boolean = left == right
+	def isSubtypeOf(left: Type.Any, right: Type.Any): Boolean = isEquivalentTo(left, right) // TODO
 
-	def isEquivalentTo(left: Type.Any, right: Type.Any): Boolean = left == right // TODO
+	def isEquivalentTo(left: Type.Any, right: Type.Any): Boolean =
+		left == right
 
 	def getLowestCommonSupertype(left: Type.Any, right: Type.Any): Type.Any = left // TODO
 
