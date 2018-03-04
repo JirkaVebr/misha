4c4a1059b4128f10e376b83197896db9b2c1c612
Jirka Vebr <jirkavebr@gmail.com>
2018-03-04 18:26:04 +0000

    Add a builtin type & some notes

---
+++ src/main/scala/com/preprocessor/spec/BlendModeType.scala
@@ -0,0 +1,30 @@
+package com.preprocessor.spec
+
+import com.preprocessor.ast.Ast.Type.{Literal, Union}
+import com.preprocessor.ast.Ast.Value
+import com.preprocessor.ast.Symbol
+
+object BlendModeType {
+
+	val map: Map[String, Symbol.TypeSymbol#Value] = Map(
+		"BlendMode" -> Union(Set(
+			Literal(Value.String("color")),
+			Literal(Value.String("color-burn")),
+			Literal(Value.String("color-dodge")),
+			Literal(Value.String("darken")),
+			Literal(Value.String("difference")),
+			Literal(Value.String("exclusion")),
+			Literal(Value.String("hard-light")),
+			Literal(Value.String("hue")),
+			Literal(Value.String("lighten")),
+			Literal(Value.String("luminosity")),
+			Literal(Value.String("multiply")),
+			Literal(Value.String("normal")),
+			Literal(Value.String("overlay")),
+			Literal(Value.String("saturation")),
+			Literal(Value.String("screen")),
+			Literal(Value.String("soft-light"))
+		))
+	)
+
+}
