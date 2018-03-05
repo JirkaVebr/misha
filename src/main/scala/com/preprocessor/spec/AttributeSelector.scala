781d7847ef9e3c74230d01116d40f9a03f98ad74
Jirka Vebr <jirkavebr@gmail.com>
2018-03-05 20:28:19 +0000

    Add basic attr selector ast

---
+++ src/main/scala/com/preprocessor/spec/AttributeSelector.scala
@@ -0,0 +1,42 @@
+package com.preprocessor.spec
+
+/**
+	* @see https://drafts.csswg.org/selectors-4/#attribute-selectors
+	*/
+object AttributeSelector {
+
+	sealed trait Modifier {
+		def name: String
+	}
+	case object CaseInsensitive extends Modifier {
+		override def name: String = "i"
+	}
+
+
+	sealed trait Matcher {
+		def symbol: String
+	}
+	case object Equals extends Matcher {
+		override def symbol: String = "=" // [attr=value]
+	}
+	case object Includes extends Matcher {
+		override def symbol: String = "~=" // [attr~=value]
+	}
+	case object Prefix extends Matcher {
+		override def symbol: String = "|=" // [attr|=value]
+	}
+	case object Preceded extends Matcher {
+		override def symbol: String = "^=" // [attr^=value]
+	}
+	case object Followed extends Matcher {
+		override def symbol: String = "=" // [attr$=value]
+	}
+	case object Contains extends Matcher {
+		override def symbol: String = "*=" // [attr*=value]
+	}
+
+	case class MatchTarget(matcher: Matcher, value: Identifier)
+
+}
+
+
