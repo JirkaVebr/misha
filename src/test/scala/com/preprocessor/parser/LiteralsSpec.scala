1624577916d70db1fbc0df0e24d886fd407f6d7d
Jirka Vebr <jirkavebr@gmail.com>
2018-03-06 00:10:10 +0000

    Move number parser test to a separate spec

--- src/test/scala/com/preprocessor/parser/LiteralsSpec.scala
+++ src/test/scala/com/preprocessor/parser/LiteralsSpec.scala
@@ -18,55 +18,6 @@ class LiteralsSpec extends BaseParserSpec {
 		assert(parse("false") == Value.Boolean(false))
 	}
 
-	it should "parse an integer literal" in {
-		assert(parse("+0") == Value.Scalar(0))
-		assert(parse("0") == Value.Scalar(0))
-		assert(parse("0000") == Value.Scalar(0))
-		assert(parse("-0") == Value.Scalar(0))
-		assert(parse("+123") == Value.Scalar(123))
-		assert(parse("123") == Value.Scalar(123))
-		assert(parse("-123") == Value.Scalar(-123))
-	}
-
-	it should "parse a fractional literal" in {
-		assert(parse("+0.00") == Value.Scalar(0))
-		assert(parse("0.00") == Value.Scalar(0))
-		assert(parse("-0.00") == Value.Scalar(0))
-		assert(parse("+123.456") == Value.Scalar(123.456))
-		assert(parse("123.456") == Value.Scalar(123.456))
-		assert(parse("-123.456") == Value.Scalar(-123.456))
-	}
-
-	it should "parse a number literal without an integral" in {
-		assert(parse("+.0") == Value.Scalar(0))
-		assert(parse("-.0") == Value.Scalar(0))
-		assert(parse("+.123") == Value.Scalar(.123))
-		assert(parse(".456") == Value.Scalar(.456))
-		assert(parse("-.456") == Value.Scalar(-.456))
-	}
-
-	it should "parse a number with an exponent" in {
-		assert(parse("10e2") == Value.Scalar(1000))
-		assert(parse("2E2") == Value.Scalar(200))
-		assert(parse("1e0") == Value.Scalar(1))
-		assert(parse("1.2e3") == Value.Scalar(1200))
-		assert(parse("-8.0e2") == Value.Scalar(-800))
-	}
-
-	it should "parse a number with a simple unit" in {
-		assert(parse("10e2s") == Value.Dimensioned(1000, UnitOfMeasure(Map("s" -> 1))))
-		assert(parse("0km") == Value.Dimensioned(0, UnitOfMeasure(Map("km" -> 1))))
-		assert(parse(".3s") == Value.Dimensioned(.3, UnitOfMeasure(Map("s" -> 1))))
-		assert(parse("-7e2ms") == Value.Dimensioned(-700, UnitOfMeasure(Map("ms" -> 1))))
-	}
-
-	it should "parse a percentage" in {
-		assert(parse("+0%") == Value.Percentage(0))
-		assert(parse("0%") == Value.Percentage(0))
-		assert(parse("123%") == Value.Percentage(123))
-		assert(parse("11.22%") == Value.Percentage(11.22))
-	}
-
 	it should "parse a hex color" in {
 		assert(parse("#123") == Rgba(17, 34, 51))
 		assert(parse("#1234") == Rgba(17, 34, 51, 68))
