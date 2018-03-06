fa924f2336293e8871c640a07082ab79586edfa9
Jirka Vebr <jirkavebr@gmail.com>
2018-03-06 19:04:53 +0000

    Re-organize rule contexts & add skeleton for some nested @rules

--- src/main/scala/com/preprocessor/ast/RuleContext.scala
+++ src/main/scala/com/preprocessor/ast/RuleContext.scala
@@ -1,13 +1,6 @@
 package com.preprocessor.ast
 
 import com.preprocessor.ast.Language.Term.MagicSymbol
-import com.preprocessor.ast.Language.Value
-import com.preprocessor.ast.Symbol.ValueSymbol
-import com.preprocessor.spec.AttributeSelector.Modifier
-import com.preprocessor.spec.PseudoClasses.NonFunctional.NonFunctionalPseudoClass
-import com.preprocessor.spec.PseudoClasses.{AnPlusB, Directionality, DropFilter}
-import com.preprocessor.spec.PseudoElements
-import com.preprocessor.spec.SelectorCombinator.Combinator
 
 object RuleContext {
 
@@ -15,43 +8,17 @@ object RuleContext {
 
 	case class RawRuleHead(components: List[Either[MagicSymbol, String]]) extends RuleContext
 
-	object Selector {
-		sealed trait Selector extends RuleContext
+	sealed trait AtRule extends RuleContext
 
-		case object Universal extends Selector
-		case class Compound(combinator: Combinator, left: Selector, right: Selector) extends Selector
+	case class CounterStyle() extends AtRule
+	case class Document() extends AtRule
+	case class FontFace() extends AtRule
+	case class FontFeatureValues() extends AtRule
+	case class Keyframes() extends AtRule
+	case class Media() extends AtRule
+	case class Page() extends AtRule
+	case class Supports() extends AtRule
+	case class Viewport() extends AtRule
 
-		case class Element(name: CssIdentifier, namespace: Option[Namespace.Namespace] = None) extends Selector
-		case class Class(name: CssIdentifier) extends Selector
-		case class Id(name: CssIdentifier) extends Selector
-
-		sealed trait PseudoClass extends Selector
-		case class SubSelector(kind: SubSelector, subSelectors: Seq[Selector]) extends PseudoClass
-		case class Dir(directionality: Directionality) extends PseudoClass
-		case class Drop(filter: Option[DropFilter]) extends PseudoClass
-		case class Lang(name: CssIdentifier) extends PseudoClass
-		case class NonFunctional(pseudoClass: NonFunctionalPseudoClass) extends PseudoClass
-		case class CustomPseudoClass(name: CssIdentifier) extends PseudoClass
-		case class Nth(kind: Nth, ab: AnPlusB, of: Option[Selector]) extends PseudoClass
-
-		sealed trait PseudoElement extends Selector
-		case class SpecifiedPseudoElement(element: PseudoElements.PseudoElement) extends PseudoElement
-		case class CustomPseudoElement(name: CssIdentifier) extends PseudoElement
-
-		// target being None signifies checking just for attribute presence
-		case class Attribute(name: String, target: Option[MatchTarget] = None, modifier: Option[Modifier] = None)
-
-	}
-
-
-	object Keyframes {
-		case class Keyframes(identifier: ValueSymbol) extends RuleContext
-
-		sealed trait Keyframe extends RuleContext
-
-		case object From extends Keyframe
-		case object To extends Keyframe
-		case class Percentage(value: Value.Percentage) extends Keyframe
-	}
 
 }
---
+++ src/main/scala/com/preprocessor/ast/Selector.scala
@@ -0,0 +1,37 @@
+package com.preprocessor.ast
+
+import com.preprocessor.spec.AttributeSelector.Modifier
+import com.preprocessor.spec.PseudoClasses
+import com.preprocessor.spec.PseudoClasses.NonFunctional.NonFunctionalPseudoClass
+import com.preprocessor.spec.PseudoClasses.{AnPlusB, Directionality, DropFilter}
+import com.preprocessor.spec.PseudoElements
+import com.preprocessor.spec.SelectorCombinator.Combinator
+
+
+object Selector {
+	sealed trait Selector
+
+	case object Universal extends Selector
+	case class Compound(combinator: Combinator, left: Selector, right: Selector) extends Selector
+
+	case class Element(name: CssIdentifier, namespace: Option[Namespace.Namespace] = None) extends Selector
+	case class Class(name: CssIdentifier) extends Selector
+	case class Id(name: CssIdentifier) extends Selector
+
+	sealed trait PseudoClass extends Selector
+	case class SubSelector(kind: SubSelector, subSelectors: Seq[Selector]) extends PseudoClass
+	case class Dir(directionality: Directionality) extends PseudoClass
+	case class Drop(filter: Option[DropFilter]) extends PseudoClass
+	case class Lang(name: CssIdentifier) extends PseudoClass
+	case class NonFunctional(pseudoClass: NonFunctionalPseudoClass) extends PseudoClass
+	case class CustomPseudoClass(name: CssIdentifier) extends PseudoClass
+	case class Nth(kind: PseudoClasses.Nth, ab: AnPlusB, of: Option[Selector]) extends PseudoClass
+
+	sealed trait PseudoElement extends Selector
+	case class SpecifiedPseudoElement(element: PseudoElements.PseudoElement) extends PseudoElement
+	case class CustomPseudoElement(name: CssIdentifier) extends PseudoElement
+
+	// target being None signifies checking just for attribute presence
+	case class Attribute(name: String, target: Option[MatchTarget] = None, modifier: Option[Modifier] = None)
+
+}
