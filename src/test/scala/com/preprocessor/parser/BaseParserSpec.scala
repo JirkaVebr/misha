9303651af08b9190abcf3b5d36594d6c95df9478
Jirka Vebr <jirkavebr@gmail.com>
2018-03-06 01:48:25 +0000

    BaseParserSpec: add helper for selector parsing

--- src/test/scala/com/preprocessor/parser/BaseParserSpec.scala
+++ src/test/scala/com/preprocessor/parser/BaseParserSpec.scala
@@ -2,6 +2,7 @@ package com.preprocessor.parser
 
 import com.preprocessor.BaseSpec
 import com.preprocessor.parser.language.LanguageParser
+import com.preprocessor.parser.selector.SelectorParser
 import org.parboiled2.{ParseError, Rule1}
 
 import scala.util.{Failure, Success}
@@ -10,6 +11,9 @@ class BaseParserSpec extends BaseSpec {
 
 	protected def parseLanguageRule[A](input: String, rule: LanguageParser => Rule1[A]): A =
 		parseRule[LanguageParser, A](LanguageParser, input, rule)
+
+	protected def parseSelectorRule[A](input: String, rule: SelectorParser => Rule1[A]): A =
+		parseRule[SelectorParser, A](SelectorParser, input, rule)
 
 
 	protected def parseRule[P <: org.parboiled2.Parser, A](parserFactory: ParserFactory[P], input: String, rule: P => Rule1[A]): A = {
