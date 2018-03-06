e1a155c8819f88ee671fa5f199bfc00831c575ec
Jirka Vebr <jirkavebr@gmail.com>
2018-03-06 00:13:01 +0000

    BaseParserSpec: loosen type constraints

--- src/test/scala/com/preprocessor/parser/BaseParserSpec.scala
+++ src/test/scala/com/preprocessor/parser/BaseParserSpec.scala
@@ -9,7 +9,7 @@ import scala.util.{Failure, Success}
 class BaseParserSpec extends BaseSpec {
 
 
-	protected def parseRule[A <: Node](input: String, rule: LanguageParser => Rule1[A]): A = {
+	protected def parseRule[A](input: String, rule: LanguageParser => Rule1[A]): A = {
 		// __run() isn't public API, so this may break
 		// @see https://groups.google.com/forum/#!topic/parboiled-user/uwcy6MVZV5s
 		val parser = LanguageParser.create(input)
