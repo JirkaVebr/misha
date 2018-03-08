343f8bec5031e8c448baede47a66a055b4951022
Jirka Vebr <jirkavebr@gmail.com>
2018-03-08 15:50:33 +0000

    Utilize cssIdentifier implicit conversions

--- src/test/scala/com/preprocessor/parser/selector/BasicsSpec.scala
+++ src/test/scala/com/preprocessor/parser/selector/BasicsSpec.scala
@@ -2,7 +2,7 @@ package com.preprocessor.parser.selector
 
 import com.preprocessor.ast
 import com.preprocessor.ast.Namespace.{AnyNamespace, NamedNamespace, NoNamespace}
-import com.preprocessor.ast.{CssIdentifier, QualifiedAttribute}
+import com.preprocessor.ast.QualifiedAttribute
 import com.preprocessor.parser.BaseParserSpec
 
 class BasicsSpec extends BaseParserSpec {
@@ -11,10 +11,10 @@ class BasicsSpec extends BaseParserSpec {
 	behavior of "Basic selector parsing"
 
 	it should "correctly parse qualified names" in {
-		assert(parseQn("foo|A") === QualifiedAttribute(CssIdentifier("A"), NamedNamespace(CssIdentifier("foo"))))
-		assert(parseQn("|B") === ast.QualifiedAttribute(CssIdentifier("B"), NoNamespace))
-		assert(parseQn("*|C") === ast.QualifiedAttribute(CssIdentifier("C"), AnyNamespace))
-		assert(parseQn("D") === ast.QualifiedAttribute(CssIdentifier("D"), NoNamespace))
+		assert(parseQn("foo|A") === QualifiedAttribute("A", NamedNamespace("foo")))
+		assert(parseQn("|B") === ast.QualifiedAttribute("B", NoNamespace))
+		assert(parseQn("*|C") === ast.QualifiedAttribute("C", AnyNamespace))
+		assert(parseQn("D") === ast.QualifiedAttribute("D", NoNamespace))
 	}
 
 
