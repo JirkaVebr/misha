561f2a1ae6de57a3336877e3f573787ab2ff50dc
Jirka Vebr <jirkavebr@gmail.com>
2018-03-08 20:18:08 +0000

    Add attr parsing tests

--- src/test/scala/com/preprocessor/parser/selector/SelectorSpec.scala
+++ src/test/scala/com/preprocessor/parser/selector/SelectorSpec.scala
@@ -1,7 +1,10 @@
 package com.preprocessor.parser.selector
 
+import com.preprocessor.ast.Namespace.AnyNamespace
+import com.preprocessor.ast.{MatchTarget, QualifiedAttribute}
 import com.preprocessor.ast.Selector._
 import com.preprocessor.parser.BaseParserSpec
+import com.preprocessor.spec.AttributeSelector._
 
 class SelectorSpec extends BaseParserSpec {
 
@@ -13,6 +16,14 @@ class SelectorSpec extends BaseParserSpec {
 
 	it should "correctly parse ids" in {
 		assert(parse("#myId") === Id("myId"))
+	}
+
+	it should "correctly parse attributes" in {
+		assert(parse("[tabindex]") === Attribute(QualifiedAttribute("tabindex")))
+		assert(parse("[|tabindex]") === Attribute(QualifiedAttribute("tabindex")))
+		assert(parse("[*|tabindex]") === Attribute(QualifiedAttribute("tabindex", AnyNamespace)))
+		assert(parse("[tabindex=123]") === Attribute(QualifiedAttribute("tabindex"), Some(MatchTarget(Equals, "123"))))
+		assert(parse("[ 	 |tabindex |=	456	]") === Attribute(QualifiedAttribute("tabindex"), Some(MatchTarget(Prefix, "456"))))
 	}
 
 
