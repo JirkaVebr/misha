ca051fd5aa0c5f80b78605c6a4f3a28ee3fb0fe8
Jirka Vebr <jirkavebr@gmail.com>
2018-03-08 15:53:10 +0000

    More apt naming

--- src/test/scala/com/preprocessor/parser/selector/BasicsSpec.scala
+++ src/test/scala/com/preprocessor/parser/selector/BasicsSpec.scala
@@ -11,13 +11,13 @@ class BasicsSpec extends BaseParserSpec {
 	behavior of "Basic selector parsing"
 
 	it should "correctly parse qualified names" in {
-		assert(parseQn("foo|A") === QualifiedAttribute("A", NamedNamespace("foo")))
-		assert(parseQn("|B") === ast.QualifiedAttribute("B", NoNamespace))
-		assert(parseQn("*|C") === ast.QualifiedAttribute("C", AnyNamespace))
-		assert(parseQn("D") === ast.QualifiedAttribute("D", NoNamespace))
+		assert(parseQan("foo|A") === QualifiedAttribute("A", NamedNamespace("foo")))
+		assert(parseQan("|B") === ast.QualifiedAttribute("B", NoNamespace))
+		assert(parseQan("*|C") === ast.QualifiedAttribute("C", AnyNamespace))
+		assert(parseQan("D") === ast.QualifiedAttribute("D", NoNamespace))
 	}
 
 
-	protected def parseQn(input: String): QualifiedAttribute = parseSelectorRule(input, _.QualifiedAttributeName)
+	protected def parseQan(input: String): QualifiedAttribute = parseSelectorRule(input, _.QualifiedAttributeName)
 
 }
