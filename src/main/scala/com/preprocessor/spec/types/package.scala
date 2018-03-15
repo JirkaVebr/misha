fc2d5f6ad6111640ac602aec70c2e6122f843158
Jirka Vebr <jirkavebr@gmail.com>
2018-03-15 16:34:43 +0000

    Properly register animatable types

--- src/main/scala/com/preprocessor/spec/types/package.scala
+++ src/main/scala/com/preprocessor/spec/types/package.scala
@@ -5,6 +5,7 @@ import com.preprocessor.ast.Symbol
 package object types {
 
 	val specTypes: Map[String, Symbol.TypeSymbol#Value] = Map(
+		Animatable.name -> Animatable(),
 		BlendMode.name -> BlendMode()
 	)
 
