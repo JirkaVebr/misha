32c8e8a6b7d7a1587ab74014dcfb991973d6070d
Jirka Vebr <jirkavebr@gmail.com>
2018-03-15 16:38:14 +0000

    Test root scope for animatable props

--- src/test/scala/com/preprocessor/interpreter/RootEnvironmentSpec.scala
+++ src/test/scala/com/preprocessor/interpreter/RootEnvironmentSpec.scala
@@ -1,6 +1,6 @@
 package com.preprocessor.interpreter
 
-import com.preprocessor.ast.Language.{Type, Value}
+import com.preprocessor.ast.Language.Type
 import com.preprocessor.ast.Language.Type.Literal
 import com.preprocessor.ast.Symbol.TypeSymbol
 
@@ -11,9 +11,13 @@ class RootEnvironmentSpec extends BaseInterpreterSpec {
 	val environment = RootEnvironment()
 
 	it should "retrieve builtin types from the spec" in {
-		assert(environment.lookup(TypeSymbol("BlendMode")).get
-			.asInstanceOf[Type.Union].subs.contains(Literal(Value.String("hue")))
-		)
+		assert(isBuiltinUnionPresent("Animatable", "margin"))
+		assert(isBuiltinUnionPresent("BlendMode", "hue"))
 	}
+
+
+	private def isBuiltinUnionPresent(name: String, exampleValue: String) =
+		environment.lookup(TypeSymbol(name)).get
+			.asInstanceOf[Type.Union].subs.contains(Literal(exampleValue))
 
 }
