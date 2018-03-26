0b69905563075b739e33c3ab1e244f63fc12923f
Jirka Vebr <jirkavebr@gmail.com>
2018-03-26 11:51:34 +0100

    Remove useless code

--- src/main/scala/com/mishaLang/interpreter/typing/Inference.scala
+++
@@ -1,9 +0,0 @@
-package com.mishaLang.interpreter.typing
-
-import com.mishaLang.ast.Language.{Type, Value}
-
-object Inference {
-
-	def inferTypeForValue(value: Value.Value): Type.Any = value.valueType // TODO
-
-}
