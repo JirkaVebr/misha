e029d04ca356b6d166a00a1f183217cdea8f2441
Jirka Vebr <jirkavebr@gmail.com>
2018-03-26 14:59:54 +0100

    Tests: move tolerant equality to base spec

--- src/test/scala/com/mishaLang/BaseSpec.scala
+++ src/test/scala/com/mishaLang/BaseSpec.scala
@@ -1,8 +1,11 @@
 package com.mishaLang
 
+import org.scalactic.{Equality, TolerantNumerics}
 import org.scalatest.FlatSpec
 
 class BaseSpec extends FlatSpec {
 
+
+	implicit val doubleEquality: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(0.00000001)
 
 }
