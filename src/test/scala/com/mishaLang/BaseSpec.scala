35661089b87b58fc21f227a17ab9e14fc1a2c659
Jirka Vebr <jirkavebr@gmail.com>
2018-03-29 02:30:49 +0100

    BaseSpec: use matchers

--- src/test/scala/com/mishaLang/BaseSpec.scala
+++ src/test/scala/com/mishaLang/BaseSpec.scala
@@ -1,9 +1,9 @@
 package com.mishaLang
 
 import org.scalactic.{Equality, TolerantNumerics}
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, Matchers}
 
-class BaseSpec extends FlatSpec {
+class BaseSpec extends FlatSpec with Matchers {
 
 
 	implicit val doubleEquality: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(0.00000001)
