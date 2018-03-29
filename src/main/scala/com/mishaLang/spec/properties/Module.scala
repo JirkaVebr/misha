b2e53964043566b34dd057ff23ae69b666d2e6de
Jirka Vebr <jirkavebr@gmail.com>
2018-03-29 21:47:38 +0100

    Add a property generator

--- src/main/scala/com/mishaLang/spec/properties/Module.scala
+++ src/main/scala/com/mishaLang/spec/properties/Module.scala
@@ -1,9 +1,22 @@
 package com.mishaLang.spec.properties
 
-import com.mishaLang.ast.Language.Value
+import com.mishaLang.ast.Language.Expression.Block
+import com.mishaLang.ast.Language.Statement.Property
+import com.mishaLang.ast.Language.Term.Variable
+import com.mishaLang.ast.Language.{Type, Value, ValueSymbolDeclaration}
+import com.mishaLang.interpreter.Scope
 
 trait Module {
 
 	def apply(): Map[String, Value.Callable]
+
+
+	protected def generateUnary(name: String, argumentType: Type.Any): Value.Lambda =
+		Value.Lambda(
+			None, Vector(ValueSymbolDeclaration[Unit](name, Some(argumentType), Unit)), Vector(),
+			None, Block(
+				Property(name, Variable(name))
+			), Scope.rootScopeId
+		)
 
 }
