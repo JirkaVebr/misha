9a37c8c88531c785ee8514c78ce1f282a336135a
Jirka Vebr <jirkavebr@gmail.com>
2018-03-30 02:14:39 +0100

    Main: measure time & print output to stdout

--- src/main/scala/com/mishaLang/Main.scala
+++ src/main/scala/com/mishaLang/Main.scala
@@ -1,15 +1,12 @@
 package com.mishaLang
 
-import java.io.PrintWriter
-
 
 object Main extends App {
 
+	val timeStart = System.nanoTime()
 	val output = Compiler.compileFile("src/scratch/test.mi")
-
-	new PrintWriter("src/scratch/out") {
-		write(output)
-		close()
-	}
+	val timeEnd = System.nanoTime()
+	println(s"Time: ${(timeEnd - timeStart) / 1e6}ms")
+	println(output)
 
 }
