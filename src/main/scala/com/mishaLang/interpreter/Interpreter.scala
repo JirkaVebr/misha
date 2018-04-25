c4f50d0d987d7a76e6c158b3cccd0254d13e55d3
Jirka Vebr <jirkavebr@gmail.com>
2018-04-25 13:37:52 +0200

    Add productChainRun for sequences of statically known sizes

--- src/main/scala/com/mishaLang/interpreter/Interpreter.scala
+++ src/main/scala/com/mishaLang/interpreter/Interpreter.scala
@@ -1,9 +1,11 @@
 package com.mishaLang.interpreter
 
 import com.mishaLang.ast.Language
-import com.mishaLang.ast.Language.{Program, Value}
+import com.mishaLang.ast.Language.Expression.Expression
 import com.mishaLang.ast.Language.Value.Value
-import shapeless.HList.ListCompat._
+import com.mishaLang.ast.Language.{Program, Value}
+import shapeless.HList._
+import shapeless._
 
 import scala.annotation.tailrec
 import scala.util.{Failure, Success, Try}
@@ -48,6 +50,36 @@ object Interpreter {
 			}
 
 		run(items, new EnvWithValues(state.environment, List.empty), evaluate)
+	}
+
+
+	type EnvWithStaticValues[+L <: HList] = AugmentedEnvironment[L]
+
+	object productChainRun extends Poly2 {
+		implicit def caseHNil[L <: HNil]: Case.Aux[L, Environment, Try[EnvWithStaticValues[L]]] =
+			at[L, Environment](
+				(hNil: L, env: Environment) => Success(AugmentedEnvironment(env, hNil))
+			)
+
+		implicit def caseHCons[H <: Expression, T <: HList, TT <: HList]
+		(implicit tailCase: Case.Aux[T, Environment, Try[EnvWithStaticValues[TT]]]):
+		Case.Aux[H :: T, Environment, Try[EnvWithStaticValues[Value :: TT]]] =
+			at[H :: T, Environment] {
+				case (expression :: tail, env) =>
+					val evaluationResult = ExpressionInterpreter.run(expression)(EnvironmentWithValue(env))
+					evaluationResult match {
+						case Failure(reason) => Failure(reason)
+						case Success(newValueEnvironment) =>
+							val environment: Environment = newValueEnvironment.environment
+							productChainRun.apply[T, Environment](tail, environment) match {
+								case Failure(reason) => Failure(reason)
+								case Success(latestState: EnvWithStaticValues[TT]) =>
+									Success(AugmentedEnvironment(
+										latestState.environment, newValueEnvironment.value :: latestState.value)
+									)
+							}
+					}
+			}
 	}
 
 }
